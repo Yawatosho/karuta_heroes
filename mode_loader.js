7a548bc89c3c47a04cac84c11bbcbf4755c50e32
Yawatosho <219481936+Yawatosho@users.noreply.github.com>
2026-08-25 22:28:29 +0900

    Add five gallery artworks

--- mode_loader.js
+++ mode_loader.js
@@ -110,7 +110,7 @@
   function loadScript(src) {
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
-      script.src = `${src}?v=fighters159`;
+      script.src = `${src}?v=fighters160`;
       script.async = false;
       script.addEventListener('load', resolve, { once: true });
       script.addEventListener('error', reject, { once: true });
